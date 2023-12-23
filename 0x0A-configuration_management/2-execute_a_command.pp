a014383224f8dfd031efd815beccf126a4fb69a3
Muigai-ben <muigaibenson37@gmail.com>
2023-12-23 02:42:40 -0800

    Create a file

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,12 @@
-# kill process killmenow
+# Define the exec resource to kill the process named "killmenow"
+exec { 'kill_killmenow_process':
+  command     => 'pkill killmenow',
+  refreshonly => true, # Only run when notified by another resource
+  subscribe   => Exec['notify_killmenow_process'], # Subscribe to a notification
+}
 
-exec { 'pkill':
-	command =>	'pkill killmenow' ,
-	provide =>	'shell' ,
+# Notify the exec resource to trigger its execution
+exec { 'notify_killmenow_process':
+  command => '/bin/true',
+  notify  => Exec['kill_killmenow_process'],
 }
