d231d9bbcbccacd6b9c044c28fc9fd369e5b59f6
Muigai-ben <muigaibenson37@gmail.com>
2023-12-23 02:33:13 -0800

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# creates a file in /tmp
-
+# creates a file in /tmP
+# Define the file resource
 file { '/tmp/school':
-	mode	=> '0744',
-	owner	=> 'www-data',
-	group	=> 'www-data',
-	content	=> 'I love Puppet'
+  ensure  => 'file',    # Ensure that it is a regular file
+  mode    => '0744',    # Set file permissions to 0744
+  owner   => 'www-data',# Set file owner to www-data
+  group   => 'www-data',# Set file group to www-data
+  content => 'I love Puppet', # Set file content
 }
